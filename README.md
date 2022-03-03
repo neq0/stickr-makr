32c1cdf96afbc8e187b0106e9ac8ca45760de569
birdue <43716901+birdue@users.noreply.github.com>
2022-03-03 21:03:53 +0800

    add: downscale gifs

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
 # stickr-makr
 
-This script converts images into stickers (i.e. downscaled images).
+A script that converts images/gifs into stickers (downscaled images/gifs).
 
-TODO
+## Example
+
+Say you have a gif that you intend to use as a reaction gif (sticker) in online group chats:
+
+![original](https://i.imgur.com/zcwHNQC.gif)
+
+However, it is 600*337px with a size of 1.4M, which kind of wastes your bandwidth every time you post it.
+
+So, you can put it inside a folder `reactions/`, execute the script with `reactions/` as an argument, then it will be downscaled to 266*149px with a reasonable size (273KB) while still being visible:
+
+![downscaled](https://i.imgur.com/n1Q4hXG.gif)
+
+Images can also be downscaled in the same fashion to reduce size.
+
+## Usage
+
+Give execute permissions:
+
+```shell
+chmod +x make-stickers.sh
+```
+
+Run:
+
+```shell
+./make-stickers.sh FOLDER
+```
+
+A new `Stickers/` folder will be created under the current working directory, containing all stickers converted from `FOLDER`. The `FOLDER` will be untouched.
+
+## Change sticker size
+
+See the `DIMENSION` variable in `make-stickers.sh`.
+
+## Requirements
+
+[ImageMagick](https://imagemagick.org/), [FFmpeg](https://ffmpeg.org/)
--- make-stickers.sh
+++ make-stickers.sh
@@ -13,7 +13,15 @@ area=$(echo $DIMENSION | awk '{ print $1 * $1 }')
 # ImageMagick is required
 command -v magick > /dev/null
 if [ $? -ne 0 ]; then
-	echo "ImageMagick is required for the use of this script."
+	echo "ImageMagick not detected."
+	echo "Exiting..."
+	exit 1
+fi
+
+# FFmpeg is required
+command -v ffmpeg > /dev/null
+if [ $? -ne 0 ]; then
+	echo "FFmpeg not detected."
 	echo "Exiting..."
 	exit 1
 fi
@@ -40,26 +48,74 @@ fi
 rm -rf Stickers/ 
 mkdir -p Stickers/
 
+# Temporary directory for storing gif frames
+tmp_dir=$(mktemp -d)
+# tmp_dir="./temp/"
+
 scale_down_image() {
 	file="$1"
 	final_extension="$2"
 	directory=$(dirname "$file")
 	# parent_directory="${file%/*}"
+	# If JPEG image, change file extension to .jpg
+	# If PNG image, do not change file extension
 	final_file="./Stickers/${file%\.*}.$final_extension"
+
 	mkdir -p "./Stickers/$directory"
+	# @: scale image area to $area; >: only downscale images
+	# Doc https://legacy.imagemagick.org/Usage/resize/#resize
 	magick convert "$IMG_DIR/$file" -resize "$area@>" "$final_file"
+	echo "Created: $final_file"
+}
+
+scale_down_gif() {
+	file="$1"
+	directory=$(dirname "$file")
+
+	rm -rf $tmp_dir/*
+
+	# Generate palette from source gif for better gif quality
+	# Copied from https://superuser.com/a/1049820/1344967
+	ffmpeg -y -i "$IMG_DIR/$file" -vf palettegen $tmp_dir/palette.png >/dev/null 2>&1
+
+	# Find fps of source gif.
+	# Copied from https://askubuntu.com/a/110269/1420906
+	fps=$(ffmpeg -i "$IMG_DIR/$file" 2>&1 | sed -n "s/.*, \(.*\) fp.*/\1/p")
+
+	mkdir $tmp_dir/frames
+	# Split source gif into frames 0001.png, 0002.png, etc
+	# ffmpeg -i "$IMG_DIR/$file" -vf "fps=$fps" $tmp_dir/frames/%04d.png
+	ffmpeg -i "$IMG_DIR/$file" $tmp_dir/frames/%04d.png >/dev/null 2>&1
+
+	# Downscale each frame
+	for frame in $tmp_dir/frames/*; do
+		frame_name=$(basename $frame)
+		magick convert "$frame" -resize "$area@>" "$tmp_dir/frames/final_$frame_name"
+	done
+
+	# ffmpeg -y -r $fps -i $tmp_dir/frames/final_%04d.png -i $tmp_dir/palette.png -filter_complex paletteuse -r $fps "./Stickers/$file"
+	mkdir -p "./Stickers/$directory"
+	# Reassemble frames into gif, set framerate, use pallete
+	ffmpeg -y -r $fps -i $tmp_dir/frames/final_%04d.png \
+		-i $tmp_dir/palette.png -filter_complex paletteuse "./Stickers/$file" >/dev/null 2>&1
+
+	echo "Created: ./Stickers/$file"
 }
 
 echo "Converting..."
+# Find all files; -printf formats filenames to be relative to starting-point instead of working directory
 files=$(find "$IMG_DIR" -mindepth 1 -type f -printf "%P\n")
 IFS=$'\n'
 for file in $files; do
-	# Match jpegs
+	# Match JPEGs
 	if [[ "$file" =~ .+\.(jpe?g|jf?if)$ ]]; then
 		scale_down_image "$file" jpg
-	# Match pngs
+	# Match PNGs
 	elif [[ "$file" =~ .+\.png$ ]]; then
 		scale_down_image "$file" png
+	# Match gifs
+	elif [[ "$file" =~ .+\.gif$ ]]; then
+		scale_down_gif "$file"
 	fi
 done
 unset IFS
