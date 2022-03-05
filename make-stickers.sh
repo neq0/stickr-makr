fcff41623d22856ed31748279558a298d5e82f0a
birdue <43716901+birdue@users.noreply.github.com>
2022-03-06 00:17:48 +0800

    Error handling: print err_msg. Bug fix: make tmp_dir under current directory

--- make-stickers.sh
+++ make-stickers.sh
@@ -37,10 +37,10 @@ fi
 # Confirmation of overwriting Stickers/
 if [[ -d Stickers/ && -n $(ls -A Stickers/) ]]; then
 	echo "The directory Stickers/ exists and is non-empty."
-	read -p "Its contents will be wiped out. Proceed? (Y/N) "
-	if [[ $REPLY != Y && $REPLY != y ]]; then
+	read 2>&1 -p "Its contents will be wiped out. Proceed? (Y/N) "
+	if [[ "$REPLY" != Y && "$REPLY" != y ]]; then
 		echo "Aborting..."
-		exit 1
+		exit 0
 	fi
 fi
 
@@ -49,14 +49,16 @@ rm -rf Stickers/
 mkdir -p Stickers/
 
 # Temporary directory for storing gif frames
-tmp_dir=$(mktemp -d)
-# tmp_dir="./temp/"
+# Ideally, use a directory that mktemp assigns.
+# However, in the case that mktemp fails - perhaps when the user lacks access to /tmp/
+# or something - make a temporary directory under ./ as a backup plan
+tmp_dir=$(mktemp -d) || tmp_dir="./.temp/"
+mkdir -p $tmp_dir
 
 scale_down_image() {
 	file="$1"
 	final_extension="$2"
 	directory=$(dirname "$file")
-	# parent_directory="${file%/*}"
 	# If JPEG image, change file extension to .jpg
 	# If PNG image, do not change file extension
 	final_file="./Stickers/${file%\.*}.$final_extension"
@@ -64,19 +66,20 @@ scale_down_image() {
 	mkdir -p "./Stickers/$directory"
 	# @: scale image area to $area; >: only downscale images
 	# Doc https://legacy.imagemagick.org/Usage/resize/#resize
-	magick convert "$IMG_DIR/$file" -resize "$area@>" "$final_file"
-	echo "Created: $final_file"
+	err_msg=$(magick convert "$IMG_DIR/$file" -resize "$area@>" "$final_file" 2>&1 >/dev/null) || return 1
 }
 
 scale_down_gif() {
 	file="$1"
 	directory=$(dirname "$file")
+	final_file="./Stickers/$file"
 
+	# Purge tmp_dir
 	rm -rf $tmp_dir/*
 
 	# Generate palette from source gif for better gif quality
 	# Copied from https://superuser.com/a/1049820/1344967
-	ffmpeg -y -i "$IMG_DIR/$file" -vf palettegen $tmp_dir/palette.png >/dev/null 2>&1
+	err_msg=$(ffmpeg -y -i "$IMG_DIR/$file" -vf palettegen $tmp_dir/palette.png 2>&1 >/dev/null) || return 1
 
 	# Find fps of source gif.
 	# Copied from https://askubuntu.com/a/110269/1420906
@@ -84,27 +87,26 @@ scale_down_gif() {
 
 	mkdir $tmp_dir/frames
 	# Split source gif into frames 0001.png, 0002.png, etc
-	# ffmpeg -i "$IMG_DIR/$file" -vf "fps=$fps" $tmp_dir/frames/%04d.png
-	ffmpeg -i "$IMG_DIR/$file" $tmp_dir/frames/%04d.png >/dev/null 2>&1
+	err_msg=$(ffmpeg -i "$IMG_DIR/$file" $tmp_dir/frames/%04d.png 2>&1 >/dev/null) || return 1
 
 	# Downscale each frame
 	for frame in $tmp_dir/frames/*; do
 		frame_name=$(basename $frame)
-		magick convert "$frame" -resize "$area@>" "$tmp_dir/frames/final_$frame_name"
+		err_msg=$(magick convert "$frame" -resize "$area@>" "$tmp_dir/frames/final_$frame_name" 2>&1 >/dev/null) || return 1
 	done
 
 	# ffmpeg -y -r $fps -i $tmp_dir/frames/final_%04d.png -i $tmp_dir/palette.png -filter_complex paletteuse -r $fps "./Stickers/$file"
 	mkdir -p "./Stickers/$directory"
 	# Reassemble frames into gif, set framerate, use pallete
-	ffmpeg -y -r $fps -i $tmp_dir/frames/final_%04d.png \
-		-i $tmp_dir/palette.png -filter_complex paletteuse "./Stickers/$file" >/dev/null 2>&1
-
-	echo "Created: ./Stickers/$file"
+	# Also copied from https://superuser.com/a/1049820/1344967
+	err_msg=$(ffmpeg -y -r $fps -i $tmp_dir/frames/final_%04d.png \
+		-i $tmp_dir/palette.png -filter_complex paletteuse "$final_file" 2>&1 >/dev/null) || return 1
 }
 
 echo "Converting..."
 # Find all files; -printf formats filenames to be relative to starting-point instead of working directory
 files=$(find "$IMG_DIR" -mindepth 1 -type f -printf "%P\n")
+old_IFS="$IFS"
 IFS=$'\n'
 for file in $files; do
 	# Match JPEGs
@@ -117,7 +119,14 @@ for file in $files; do
 	elif [[ "$file" =~ .+\.gif$ ]]; then
 		scale_down_gif "$file"
 	fi
+
+	# Error handling
+	if [ $? -eq 0 ]; then
+		echo "Created: $final_file"
+	else
+		>&2 echo -e "Failed to convert "$(realpath $IMG_DIR/$file)". Error message:\n$err_msg"
+	fi
 done
-unset IFS
+IFS="$old_IFS"
 
 echo "Conversion completed"
